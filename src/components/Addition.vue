0eb9c42e1c50167bc3e9096bc60e6c86b3da2a40
沐雪 <48502416+lisbeth0720@users.noreply.github.com>
2022-05-31 19:05:59 +0800

    init

--- src/components/Addition.vue
+++ src/components/Addition.vue
@@ -29,10 +29,12 @@ export default {
       // this.$store.state.count++;
 
       //vuex中推荐在Mutitions中修改state中的数据，不推荐在组件中直接通过this.$store来修改
+      
       //触发mutations中的第一种方式
       //commit()的作用就是触发调用某个mutations
 
       //1.this.$store.commit()是触发mutations的第一种方式
+      
       //commit的作用就是调用某个mutation函数
        this.$store.commit('add')
        //2.触发mutations的第二种方式
@@ -64,24 +66,27 @@ export default {
        //2.2通过刚导入的mapState函数，将当前组件需要的全局数据映射为当前组件的methods
        //2.2将全局数据映射为当前组件中的methods-在Subtration组件中用了
 
-
       //新需求，点击+1，1秒后+1，用了setTimeOut，由于setTimeOut是异步，所以需要在action里执行
       //1.触发action的第一种方式 dispatch
        this.$store.dispatch('addAsync')
     },
     
     btnHandle22(){
+
       //携带参数
       //新需求，点击+1，1秒后+1，用了setTimeOut，由于setTimeOut是异步，所以需要在action里执行
       //触发action的第一种方式 dispatch
        this.$store.dispatch('addNAsync',this.n)
+       
     },
   }
 }
 </script>
 
 <style>
+
     div button{
       margin-left:10px;
     }
+    
 </style>
